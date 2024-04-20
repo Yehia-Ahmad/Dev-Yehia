268264563611e388057175ea210766fa3be91a33
Yehia <Yehia>
2024-04-20 13:37:40 +0200

    sutep project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  title = 'Dev Yehia';
+  isDarkMode: boolean = true;
+  isChrome: boolean = true;
+  classes: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.isChrome = navigator.userAgent.includes("Chrome");
+    this.classes = {
+      'dark-mode': this.isDarkMode,
+      'light-mode': !this.isDarkMode,
+      'mozilla-style': !this.isChrome
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    ProjectsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
