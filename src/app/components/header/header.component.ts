3797da116f6e46ea4188706dca1696f29931d85d
Yehia <Yehia>
2024-04-22 18:48:11 +0200

    Add Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DarkModeService } from 'src/app/services/dark-mode.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  classes: any;
+  classesSubtitle: any;
+  classesSecndary: any;
+  classesBorder: any;
+  isDarkMode: any;
+  hideListMenu: boolean = true;
+
+  constructor(private darkModeService: DarkModeService, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'menu',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/menu.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'moon',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/moon.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'sun',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/sun.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'close',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/close.svg')
+    );
+  }
+
+  ngOnInit() {
+    this.darkModeService.isDarkMode.subscribe((res: boolean) => {
+      this.isDarkMode = res;
+      this.classes = {
+        'dark-mode': this.isDarkMode,
+        'light-mode': !this.isDarkMode,
+      };
+
+      this.classesSubtitle = {
+        'dark-mode-subtitle': this.isDarkMode,
+        'light-mode-subtitle': !this.isDarkMode,
+      };
+
+      this.classesSecndary = {
+        'dark-mode-secondary': this.isDarkMode,
+        'light-mode-secondary': !this.isDarkMode,
+      };
+      this.classesBorder = {
+        'dark-mode-border': this.isDarkMode,
+        'light-mode-border': !this.isDarkMode,
+      };
+    });
+  }
+
+  toogleMode() {
+    this.darkModeService.isDarkMode.next(!this.isDarkMode);
+    this.isDarkMode = this.darkModeService.isDarkMode.value;
+    this.darkModeService.setItem('isDarkMode', this.isDarkMode);
+  }
+
+  toogleListMenu() {
+    this.hideListMenu = !this.hideListMenu;
+  }
 
 }
