49198dde1efb771dbba089b266d998707a15fce0
Yehia <Yehia>
2024-04-22 22:22:54 +0200

    Add project section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { myProjects } from 'src/app/core/projectsObject';
+import { DarkModeService } from 'src/app/services/dark-mode.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  all: boolean = true;
+  js: boolean = false;
+  an: boolean = false;
+  re: boolean = false;
+  classesBlue: any;
+  classesTitle: any;
+  classesSubtitle: any;
+  classesIcons: any;
+  isDarkMode: any;
+  allProjects = myProjects;
+
+  constructor(private darkModeService: DarkModeService, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'github',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg')
+    );
+  }
+
+  ngOnInit() {
+    this.darkModeService.isDarkMode.subscribe((res: boolean) => {
+      this.isDarkMode = res;
+      this.classesTitle = {
+        'dark-mode-title': this.isDarkMode,
+        'light-mode-title': !this.isDarkMode,
+      };
+      this.classesBlue = {
+        'dark-mode-blue': this.isDarkMode,
+        'light-mode-blue': !this.isDarkMode,
+      };
+      this.classesSubtitle = {
+        'dark-mode-subtitle': this.isDarkMode,
+        'light-mode-subtitle': !this.isDarkMode,
+      };
+      this.classesIcons = {
+        'dark-mode-icons': this.isDarkMode,
+        'light-mode-icons': !this.isDarkMode,
+      };
+    });
+  }
+
+  toggleActiveButton(button: string) {
+    switch (button) {
+      case 'all':
+        this.all = true; this.js = false; this.an = false; this.re = false;
+        break;
+      case 'js':
+        this.all = false;
+        this.js = true;
+        this.an = false;
+        this.re = false;
+        break;
+      case 'an':
+        this.all = false;
+        this.js = false;
+        this.an = true;
+        this.re = false;
+        break;
+      case 're':
+        this.all = false;
+        this.js = false;
+        this.an = false;
+        this.re = true;
+        break;
+    }
+  }
+
+  openProject(project: any) {
+    window.open(project.link, '_blank');
+  }
+
+  openProjectSource(project: any) {
+    window.open(project.githubLink, '_blank');
+  }
 
 }
---
+++ src/app/core/projectsObject.ts
@@ -0,0 +1,40 @@
+export const myProjects = [
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "../../assets/images/1.jpg",
+  },
+  {
+    projectTitle: "react & css project",
+    category: ["react", "css"],
+    imgPath: "../../assets/images/2.jpg",
+  },
+  { projectTitle: "css project", category: ["css"], imgPath: "../../assets/images/3.jpg" },
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "../../assets/images/4.jpg",
+  },
+  { projectTitle: "css project", category: ["css"], imgPath: "../../assets/images/5.jpg" },
+
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "../../assets/images/6.jpg",
+  },
+  {
+    projectTitle: "node project",
+    category: ["node"],
+    imgPath: "../../assets/images/7.jpg",
+  },
+  {
+    projectTitle: "node project",
+    category: ["node"],
+    imgPath: "../../assets/images/8.jpg",
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "../../assets/images/9.jpg",
+  },
+];
