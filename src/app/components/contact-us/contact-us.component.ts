ff4ad47e4c08021ff7c26bf334cb2c3a16b0ff15
Yehia <Yehia>
2024-04-22 23:26:42 +0200

    Add contact section

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DarkModeService } from 'src/app/services/dark-mode.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
+  classesBlue: any;
+  classesIcons: any;
+  classesTitle: any;
+  classesSubtitle: any;
+  isDarkMode: any;
+  classesInput: any;
+
+  constructor(private darkModeService: DarkModeService, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'mail',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/mailme.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'mail-animation',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/mail-animation.svg')
+    );
+  }
+
+  ngOnInit(): void {
+    this.darkModeService.isDarkMode.subscribe((res: boolean) => {
+      this.isDarkMode = res;
+      this.classesTitle = {
+        'dark-mode-title': this.isDarkMode,
+        'light-mode-title': !this.isDarkMode,
+      };
+      this.classesSubtitle = {
+        'dark-mode-subtitle': this.isDarkMode,
+        'light-mode-subtitle': !this.isDarkMode,
+      };
+      this.classesInput = {
+        'dark-mode-input': this.isDarkMode,
+        'light-mode-input': !this.isDarkMode,
+      };
+    });
+  }
 
 }
